e927d1f6b7c4d68ca396a24c32c61bdc3cb85dd0
dantr <huynhngdantr@gmail.com>
2023-09-21 20:50:20 +0700

    make validation DTO, allow whitelist

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //only get value from dto define
   await app.listen(3000);
 }
 bootstrap();
