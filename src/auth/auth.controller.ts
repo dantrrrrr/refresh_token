e7fa8019b90574cfb4cd7bd6bd21bfd3dbfee3e9
dantr <huynhngdantr@gmail.com>
2023-09-21 22:54:59 +0700

    custom auth class

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,17 +6,19 @@ import {
   Post,
   UseGuards,
   Req,
+  Logger,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { AtGuard, RtGuard } from 'src/common/guards';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
+  private readonly logger = new Logger(AuthController.name);
   @Post('local/signup')
   @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
@@ -29,19 +31,20 @@ export class AuthController {
     return this.authService.signinLocal(dto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AtGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   logout(@Req() req: Request) {
     const user = req.user;
-    console.log(
-      '🚀 ~ file: auth.controller.ts:37 ~ AuthController ~ logout ~ user:',
-      user,
-    );
+    this.logger.log(`User ${user?.['sub']} is logging out`);
+    // console.log(
+    //   '🚀 ~ file: auth.controller.ts:37 ~ AuthController ~ logout ~ user:',
+    //   user,
+    // );
     return this.authService.logout(user['sub']);
   }
 
-  @UseGuards(AuthGuard('jwt-refresh'))
+  @UseGuards(RtGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
   refreshTokens(@Req() req: Request) {
